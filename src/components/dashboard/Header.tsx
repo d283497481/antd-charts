aed91acf8ceca9178c75eac9121fbf2115b6ed89
arjansunar <arjan.gahatraj@gmail.com>
2022-11-26 11:10:04 +0545

    adding pie charts

--- src/components/dashboard/Header.tsx
+++ src/components/dashboard/Header.tsx
@@ -1,18 +1,25 @@
 import { FC } from "react";
 
+const colors = ["red", "violet", "emerald", "orange", "amber"] as const;
+
+const headerColorsDict = [
+  "text-red-600 bg-red-200",
+  "text-violet-600 bg-violet-200",
+  "text-emerald-600 bg-emerald-200",
+  "text-orange-600 bg-orange-200",
+  "text-amber-600 bg-amber-200",
+] as const;
 interface Props {
   title: string;
-  color?: string;
+  color?: typeof headerColorsDict[number];
 }
 
 export const DashCardHeader: FC<Props> = ({ title, color }) => {
   return (
     <span
       className={`${
-        color
-          ? `text-${color}-600 bg-${color}-200`
-          : "text-blue-600 bg-blue-200"
-      }  mb-1 font-mono  px-3 py-2 rounded-t block `}
+        color ? color : "text-blue-600 bg-blue-200"
+      }  mb-1 font-mono  px-3 py-2 rounded-t block capitalize`}
     >
       {title}
     </span>
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from "./LineChart";
 export * from "./MultiLineChart";
 export * from "./dashboard";
 export * from "./AreaChart";
+export * from "./PieChart";
